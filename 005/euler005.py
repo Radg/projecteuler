3181a211e0cbd193897722e0ff0ec50083ad513d
Radg <radic@radic.ru>
2016-08-26 12:57:21 +0400

    Reduce computing time to 7s

--- 005/euler005.py
+++ 005/euler005.py
@@ -4,13 +4,13 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-sp = i = j = 1
+i, sp = 1, 20
 found = False
 
 while not found:
-	for i in range(2, 20):
+	for i in range(11, 20):
 		if (sp % i) != 0:
-			sp += 1
+			sp += 20
 			break
 	else:
 		found = True
