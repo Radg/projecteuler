75aa760dd97f1362ba6b03b63122bf3e797a04a9
Radg <radic@radic.ru>
2016-08-27 15:35:58 +0400

    empty commit

--- 006/euler006.py
+++ 006/euler006.py
@@ -11,9 +11,4 @@
 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-sumofsqares = squareofsums = i = 0
-
-for i in range(1, 101):
-	sumofsqares += i ** 2
-
 print sum(range(1, 101)) ** 2 - sum(x ** 2 for x in range(1, 101))
