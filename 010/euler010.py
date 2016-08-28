95d56b4af9e4bc456eb0f26a27e25619f74211f7
Radg <radic@radic.ru>
2016-08-28 23:39:23 +0400

    Sieve of Eratosphenes with generator

--- 010/euler010.py
+++ 010/euler010.py
@@ -4,13 +4,13 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
-sum, n = 2, 2000000
+def primes_soe(num):
+	# Sieve of Eratosphenes realisation via generator
+    sieveList = [False] * 2 + [True] * (num - 2) 
+    for (i, isPrime) in enumerate(sieveList):
+        if isPrime:
+            yield i
+            for n in xrange(i*i, num, i):
+                sieveList[n] = False
 
-for i in range(3, n+1, 2):
-    for j in range(3, int(i ** .5) + 1):
-        if i % j == 0:
-             break
-    else:
-        sum += i
-
-print sum
+print sum(primes_soe(2000000))
