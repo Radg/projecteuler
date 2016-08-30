58984d4868c7cea0a1fbe75868965444fb7ae141
Radg <radic@radic.ru>
2016-08-30 23:43:15 +0400

    Brute force with reduce, corrected index range

--- 011/euler011.py
+++ 011/euler011.py
@@ -52,18 +52,16 @@ matrix = [[8,  02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 7
           [20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54],
           [01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48]]
 
-rng = [0, 1, 2, 3]
-
-for i in xrange(3, 16):
-	for j in xrange(3, 16):
-		product = max(reduce(mul, (matrix[i+n][j] for n in rng)),
-			          reduce(mul, (matrix[i-n][j] for n in rng)),
-			          reduce(mul, (matrix[i][j+n] for n in rng)),
-			          reduce(mul, (matrix[i][j-n] for n in rng)),
-			          reduce(mul, (matrix[i-n][j-n] for n in rng)),
-			          reduce(mul, (matrix[i+n][j+n] for n in rng)),
-			          reduce(mul, (matrix[i-n][j+n] for n in rng)),
-			          reduce(mul, (matrix[i+n][j-n] for n in rng)))
+for i in xrange(3, 17):
+	for j in xrange(3, 17):
+		product = max(reduce(mul, (matrix[i+n][j] for n in [0, 1, 2, 3])),
+			          reduce(mul, (matrix[i-n][j] for n in [0, 1, 2, 3])),
+			          reduce(mul, (matrix[i][j+n] for n in [0, 1, 2, 3])),
+			          reduce(mul, (matrix[i][j-n] for n in [0, 1, 2, 3])),
+			          reduce(mul, (matrix[i-n][j-n] for n in [0, 1, 2, 3])),
+			          reduce(mul, (matrix[i+n][j+n] for n in [0, 1, 2, 3])),
+			          reduce(mul, (matrix[i-n][j+n] for n in [0, 1, 2, 3])),
+			          reduce(mul, (matrix[i+n][j-n] for n in [0, 1, 2, 3])))
 		maxproduct = product if product > maxproduct else maxproduct
 
 print maxproduct
