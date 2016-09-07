4025ef70939976b6489208f004acac8d96aecf43
Radg <radic@radic.ru>
2016-09-08 00:59:46 +0400

    Dinamic solution with pretreated grid, cleared code

--- 015/euler015.py
+++ 015/euler015.py
@@ -12,39 +12,8 @@ grid = [[0]*gridSize for i in range(gridSize)]
 for i in xrange(gridSize - 1):
 	grid[i][gridSize-1] = grid[gridSize-1][i] = 1
 
-# print grid
-
-# grid = [[0] * (gridSize - 1) + [1]] * (gridSize - 1) + [[1] * gridSize]
-
-def routesNum(n, m):
-	if n == 1 or m == 1: 
-		grid[n-1][m-1] = 1
-		return 1
-	if grid[n-1][m-1]:
-		return grid[n-1][m-1]
-	else:
-		grid[n-1][m-1] = routesNum(n - 1, m) + routesNum(n, m - 1)
-		return grid[n-1][m-1]
-
-
 for i in xrange(gridSize - 2, -1, -1):
 	for j in xrange(i, -1, -1):
 		grid[i][j] = grid [j][i] = grid[i+1][j] + grid[i][j+1]
-	print gridSize-i, ": ", grid[i][i]
 
 print grid[0][0]
-
-
-# for i in xrange(0, gridSize):
-
-# for i in xrange(0, 5):
-# 	grid[i][gridSize - 2] = 1
-# 	print grid
-
-# print routesNum(gridSize, gridSize)
-# print grid
-
-# for i in xrange(1, 6):
-# 	print ("Routes for %d x %d grid is: %d " % (i, i, routesNum(i, i)))
-
-# print grid
